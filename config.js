dd80d82f5e4b99c9cb9958f1d0069054beeadcf8
Hector Luevano <hecth77@gmail.com>
2020-08-29 15:37:06 -0500

    Preparing to Heroku 2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/spaceplannerdb',
+    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/spaceplannerdb',
     SECRET_TOKEN: 'spaceplanner777TortasMcFly'
 }
