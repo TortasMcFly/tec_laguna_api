27925daa558f366da9f1f905ff7fd73bf77728cd
Hector Luevano <hecth77@gmail.com>
2020-11-03 16:18:46 -0600

    Cors added:

--- app.js
+++ app.js
@@ -3,8 +3,10 @@
 const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
+const cors = require('cors')
 const api = require('./routes')
 
+app.use(cors())
 app.use(bodyparser.urlencoded( {extended: false} ))
 app.use(bodyparser.json())
 app.use('/api', api)
